3e77d30e1eac29323fba5bf9e6b317d21d355735
Margaret Morris <margaretblue@gmail.com>
2014-09-07 20:52:53 -0600

    Initial Commit

---
+++ fizzie-o/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fizzie-o
+//
+//  Created by MARGARET MORRIS on 9/7/14.
+//  Copyright (c) 2014 Margaret Blue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
